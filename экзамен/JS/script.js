f9b4a89b1aba6b9788c49e9262cbb741c4fa5b66
altai04 <116072835+altai04@users.noreply.github.com>
2022-10-18 18:04:14 +0600

    Add files via upload

---
+++ экзамен/JS/script.js
@@ -0,0 +1,24 @@
+let age =   prompt("сколько тебе лет золотце")
+
+if(age < 6 ){
+
+    document.write(" <h1>здрасте мальыш")
+}
+
+else if(age > 6 && age < 12){
+document.write(" <h1>мальчишка")
+
+}
+
+else if(
+    age > 12 && age < 18
+){
+    document.write(" <h1> здорова чувак")
+    
+}
+
+else if (age>18 && age < 60 ){
+
+
+    document.write(" <h1> МУЖИК")
+}
